d64ad234c1ce1cb2a283af58091fa5067c8adbb0
Camilo Orozco HDZ <camilo.orozcohdz@gmail.com>
2022-08-02 00:49:56 -0500

    nuevo commit

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 
 const Login = () => {
@@ -6,7 +7,9 @@ const Login = () => {
   return (
     <div>
       <span>Click para ir al home</span>
-      <button>Login</button>
+      <Link to={'/home'}>
+        <button>Login</button>
+      </Link>
     </div>
   )
 }
